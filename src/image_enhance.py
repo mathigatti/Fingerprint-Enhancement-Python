f46177a5df5d62f13520962d6b6504b791af2aa2
Mathi Gatti <mathigatti@gmail.com>
2020-03-10 20:19:48 -0300

    updates

--- src/image_enhance.py
+++ src/image_enhance.py
@@ -10,10 +10,9 @@ from ridge_freq import ridge_freq
 from ridge_filter import ridge_filter
 
 def image_enhance(img):
-    blksze = 16;
+    blksze = 64;
     thresh = 0.1;
     normim,mask = ridge_segment(img,blksze,thresh);             # normalise the image and find a ROI
-
 
     gradientsigma = 1;
     blocksigma = 7;
